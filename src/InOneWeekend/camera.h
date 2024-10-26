beb39a59b97458da56403361d25e9938d4c79331
Cliiive <sasowski@outlook.de>
2024-10-26 17:57:12 +0200

    extracted render line into its own function

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -37,14 +37,7 @@ class camera {
 
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-            for (int i = 0; i < image_width; i++) {
-                color pixel_color(0,0,0);
-                for (int sample = 0; sample < samples_per_pixel; sample++) {
-                    ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, max_depth, world);
-                }
-                write_color(std::cout, pixel_samples_scale * pixel_color);
-            }
+            render_line(j, world);
         }
 
         std::clog << "\rDone.                 \n";
@@ -60,6 +53,17 @@ class camera {
     vec3   u, v, w;              // Camera frame basis vectors
     vec3   defocus_disk_u;       // Defocus disk horizontal radius
     vec3   defocus_disk_v;       // Defocus disk vertical radius
+
+    void render_line(int j, const hittable& world) {
+        for (int i = 0; i < image_width; i++) {
+            color pixel_color(0,0,0);
+            for (int sample = 0; sample < samples_per_pixel; sample++) {
+                ray r = get_ray(i, j);
+                pixel_color += ray_color(r, max_depth, world);
+            }
+            write_color(std::cout, pixel_samples_scale * pixel_color);
+        }
+    }
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
